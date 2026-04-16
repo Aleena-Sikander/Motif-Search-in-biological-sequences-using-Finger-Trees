8671c4409e5b427b39c1a3b4295ba767e9a99650
Aleena Sikandar <as08581@st.habib.edu.pk>
2026-04-16 10:11:17 +0500

    added comments

--- Utils.h
+++ Utils.h
@@ -5,7 +5,7 @@
 
 class Utils {
 public:
-    static bool isValidDNA(char c);
+    static bool isValidDNA(char c); //to check if this letter is a valid DNA char or not
     static std::string toUpper(const std::string& str);
 };
 
